c045300efcc98952e41f96d807c450f4c7cb0158
Atharvkadam15 <atharvkadam9996@gmail.com>
2026-02-07 10:48:16 +0530

    Add files via upload

---
+++ Day8/p1.java
@@ -0,0 +1,11 @@
+public class p1 {
+    public static void main(String[] args) {
+        int prev2=0;
+        int prev1=1;
+
+        System.out.println(prev2);
+        System.out.println(prev1);
+
+        for
+    }
+}
